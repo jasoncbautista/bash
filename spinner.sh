261f9a8ae82354bb766db0dd060ce4a642972f97
Jason <bjason@gmail.com>
2013-11-15 00:46:29 -0800

    spinner kinda works

--- spinner.sh
+++ spinner.sh
@@ -31,10 +31,10 @@ rotdash2(){
     p=$1
     while [ -d /proc/$p ]
     do
-        echo -en '\E[32;40m/^H' ; sleep .05
-        echo -en '\E[32;40m-^H' ; sleep .05
-        echo -en '\E[32;40m\^H' ; sleep .05
-        echo -en '\E[32;40m|^H' ; sleep .05
+        echo -en '\E[32;40m/' ; sleep .05
+        echo -en '\E[32;40m-' ; sleep .05
+        echo -en '\E[32;40m\' ; sleep .05
+        echo -en '\E[32;40m|' ; sleep .05
         echo -en '\E[32;42mS' ; tput sgr0 ; sleep .2
     done
 }
@@ -55,5 +55,5 @@ sleep 10 &
 
 last_pid=$1
 echo "$last_pid"
-spinner $!
+rotdash2 $!
 
