b1ca5a10113f0e858d1074d55ee6b187b3bce397
Jason <bjason@gmail.com>
2013-11-15 01:01:58 -0800

    fixed other spinner

--- spinner.sh
+++ spinner.sh
@@ -14,28 +14,28 @@
 spinner(){
     PROC=$1
     while [ -d /proc/$PROC ];do
-        echo -n '/^H' ; sleep 0.05
-        echo -n '-^H' ; sleep 0.05
-        echo -n '\^H' ; sleep 0.05
-        echo -n '|^H' ; sleep 0.05
+        echo -n '/' ; sleep 0.05
+        echo -n '-' ; sleep 0.05
+        echo -n '\' ; sleep 0.05
+        echo -n '|' ; sleep 0.05
     done
     return 0
 }
 
-#du /usr >/dev/null 2>&1 &
-# spinner $(pidof du)
+du /usr >/dev/null 2>&1 &
+spinner $(pidof du)
 
 
 
 rotdash2(){
     p=$1
+    echo "$p"
     while [ -d /proc/$p ]
     do
 	echo -n '/' ; sleep 0.07
 	echo -n '*' ; sleep 0.07
 	echo -n '\' ; sleep 0.07
 	echo -n '|' ; sleep 0.07
-
     done
 }
 
@@ -51,7 +51,6 @@ function spinner {
     return 0
 }
 
-sleep 10 &
-
-rotdash2 $!
+#sleep 10 &
+#rotdash2 $!
 
